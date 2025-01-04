265a0c3962c37b8d64dac82cde489116fd42de94
Quan <doquan020903@gmail.com>
2025-01-04 13:49:08 +0700

    feat: add detail post card

--- app/forum/HashtagTopicCard.tsx
+++ app/forum/HashtagTopicCard.tsx
@@ -5,26 +5,68 @@ import { MaterialCommunityIcons } from '@expo/vector-icons'; // You can use Mate
 interface HashtagTopicCardProps {
   hashtag: string;
   description: string;
+  isSubscribed: boolean;
   onCreatePost: () => void;
+  onToggleSubscription: () => void;
 }
 
 const HashtagTopicCard: React.FC<HashtagTopicCardProps> = ({
   hashtag,
   description,
+  isSubscribed,
   onCreatePost,
+  onToggleSubscription,
 }) => {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text style={styles.hashtagText}>#{hashtag}</Text>
-        <TouchableOpacity onPress={onCreatePost} style={styles.button}>
-          <View style={styles.buttonContent}>
-            <Text style={styles.buttonText}>Tạo bài đăng</Text>
-            <MaterialCommunityIcons name="plus" size={24} color="white" />
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            alignItems: 'flex-start',
+            flex: 1, // Ensures the sections take up equal space
+          }}
+        >
+          <View
+            style={{
+              flex: 1,
+              justifyContent: 'space-between',
+            }}
+          >
+            <Text style={styles.hashtagText}>#{hashtag}</Text>
+            <Text style={styles.description}>{description}</Text>
           </View>
-        </TouchableOpacity>
+
+          <View
+            style={{
+              flex: 1,
+              alignItems: 'flex-end',
+              justifyContent: 'space-between',
+            }}
+          >
+            <TouchableOpacity
+              style={[
+                styles.subscriptionButton,
+                isSubscribed
+                  ? styles.unsubscribeButton
+                  : styles.subscribeButton,
+              ]}
+              onPress={onToggleSubscription}
+            >
+              <Text style={styles.subscriptionButtonText}>
+                {isSubscribed ? 'Unsubscribe' : 'Subscribe'}
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={onCreatePost} style={styles.button}>
+              <View style={styles.buttonContent}>
+                <Text style={styles.buttonText}>Create Post</Text>
+                <MaterialCommunityIcons name="plus" size={24} color="white" />
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
-      <Text style={styles.description}>{description}</Text>
     </View>
   );
 };
@@ -32,6 +74,13 @@ const HashtagTopicCard: React.FC<HashtagTopicCardProps> = ({
 const styles = StyleSheet.create({
   container: {
     padding: 16,
+    backgroundColor: 'white',
+    borderRadius: 8,
+    shadowColor: '#000',
+    shadowOpacity: 0.5,
+    elevation: 5,
+    borderWidth: 1,
+    marginVertical: 6,
   },
   header: {
     flexDirection: 'row',
@@ -46,7 +95,7 @@ const styles = StyleSheet.create({
   button: {
     paddingVertical: 4,
     paddingHorizontal: 24,
-    backgroundColor: '#6200ea', // You can change the button color
+    backgroundColor: '#7fbbf0',
     borderRadius: 20,
     flexDirection: 'row',
     alignItems: 'center',
@@ -62,6 +111,22 @@ const styles = StyleSheet.create({
   description: {
     marginTop: 8,
     color: 'gray',
+  },
+  subscriptionButton: {
+    padding: 12,
+    borderRadius: 8,
+    alignSelf: 'flex-end',
+    marginBottom: 16,
+  },
+  subscribeButton: {
+    backgroundColor: 'blue',
+  },
+  unsubscribeButton: {
+    backgroundColor: 'gray',
+  },
+  subscriptionButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
 
--- app/forum/PostCard.tsx
+++ app/forum/PostCard.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { useRouter } from 'expo-router';
+import ImageViewer from 'react-native-image-viewing';
+import { ActivityIndicator } from 'react-native';
 
 interface Post {
   hashtags: string[];
-  applicationUser: { userName: string } | null;
   createdAt: Date;
   content: string;
   imageUrls: string[];
   likes: number;
+  userName: string;
   comments: string[];
 }
 
@@ -15,22 +19,133 @@ interface PostItemProps {
   postData: Post;
   onLikePressed: () => void;
   onUnlikePressed: () => void;
-  onCommentPressed: () => void;
 }
 
 const PostItem: React.FC<PostItemProps> = ({
   postData,
   onLikePressed,
   onUnlikePressed,
-  onCommentPressed,
 }) => {
+  const [isLiked, setIsLiked] = useState(false);
+  const [isViewerVisible, setIsViewerVisible] = useState(false);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [randomAvatarUrl, setRandomAvatarUrl] = useState('');
+  const router = useRouter();
+
+  useEffect(() => {
+    // Random avatar URL
+    setRandomAvatarUrl(
+      `https://avatar.iran.liara.run/public/${Math.floor(Math.random() * 100) + 1}`
+    );
+  }, []);
+
+  const toggleLike = () => {
+    if (isLiked) {
+      onUnlikePressed();
+    } else {
+      onLikePressed();
+    }
+    setIsLiked(!isLiked);
+  };
+
+  const navigateToComments = () => {
+    router.push({
+      pathname: '/forum/comment-page',
+      params: { params: JSON.stringify(postData) },
+    });
+  };
+
+  const handleImagePress = (index: number) => {
+    setCurrentImageIndex(index);
+    setIsViewerVisible(true);
+  };
+
+  const closeImageViewer = () => {
+    setIsViewerVisible(false);
+  };
+
+  const renderImageLayout = () => {
+    const imageCount = postData.imageUrls.length;
+
+    if (imageCount === 1) {
+      const imageUrl = postData.imageUrls[0];
+      return (
+        <View style={styles.fullImageContainer}>
+          <TouchableOpacity onPress={() => handleImagePress(0)}>
+            <Image
+              source={{ uri: imageUrl }}
+              style={styles.firstImage}
+              onError={(e) => console.log('Image error', e.nativeEvent.error)}
+            />
+          </TouchableOpacity>
+        </View>
+      );
+    }
+
+    if (imageCount === 2) {
+      return (
+        <View style={styles.twoImagesContainer}>
+          {postData.imageUrls.map((url, index) => (
+            <TouchableOpacity
+              key={index}
+              onPress={() => handleImagePress(index)}
+              style={styles.twoImageWrapper}
+            >
+              <Image source={{ uri: url }} style={styles.otherImage} />
+            </TouchableOpacity>
+          ))}
+        </View>
+      );
+    }
+
+    if (imageCount >= 3) {
+      return (
+        <View style={styles.threeImagesContainer}>
+          <TouchableOpacity
+            onPress={() => handleImagePress(0)}
+            style={styles.firstImageWrapper}
+          >
+            <Image
+              source={{ uri: postData.imageUrls[0] }}
+              style={styles.firstImage}
+            />
+          </TouchableOpacity>
+          <View style={styles.otherImagesWrapper}>
+            {postData.imageUrls.slice(1, 3).map((url, index) => (
+              <TouchableOpacity
+                key={index}
+                onPress={() => handleImagePress(index + 1)}
+                style={styles.otherImageWrapper}
+              >
+                <Image source={{ uri: url }} style={styles.otherImage} />
+              </TouchableOpacity>
+            ))}
+          </View>
+        </View>
+      );
+    }
+  };
+
   return (
     <View style={styles.container}>
       {/* User Info */}
+
       <View style={styles.userInfo}>
-        <Text style={styles.username}>
-          {postData.applicationUser?.userName || 'Anonymous'}
-        </Text>
+        <View style={{ flexDirection: 'row', alignItems: 'center', flex: 1 }}>
+          {randomAvatarUrl ? (
+            <Image source={{ uri: randomAvatarUrl }} style={styles.avatar} />
+          ) : (
+            <ActivityIndicator
+              size="small"
+              color="#7fbbf0"
+              style={styles.avatar}
+            />
+          )}
+          <Text style={styles.username}>
+            {postData.userName ? postData.userName : 'Anonymous'}
+          </Text>
+        </View>
+
         <Text style={styles.createdAt}>
           {new Date(postData.createdAt).toLocaleDateString()}
         </Text>
@@ -40,39 +155,44 @@ const PostItem: React.FC<PostItemProps> = ({
       <Text style={styles.content}>{postData.content}</Text>
 
       {/* Images */}
-      {postData.imageUrls.length > 0 && (
-        <View style={styles.imageContainer}>
-          {postData.imageUrls.map((url, index) => (
-            <Image key={index} source={{ uri: url }} style={styles.image} />
-          ))}
-        </View>
-      )}
+      {postData.imageUrls.length > 0 && renderImageLayout()}
 
       {/* Hashtags */}
-      <View style={styles.hashtags}>
+      <View style={styles.hashtagsContainer}>
         {postData.hashtags.map((hashtag, index) => (
-          <Text key={index} style={styles.hashtag}>
-            #{hashtag}
-          </Text>
+          <View key={index} style={styles.tag}>
+            <Text style={styles.hashtag}>#{hashtag}</Text>
+          </View>
         ))}
       </View>
 
       {/* Actions */}
       <View style={styles.actions}>
-        <TouchableOpacity onPress={onLikePressed} style={styles.actionButton}>
-          <Text style={styles.actionText}>Like</Text>
+        <TouchableOpacity onPress={toggleLike} style={styles.likeButton}>
+          <Ionicons
+            name={isLiked ? 'heart' : 'heart-outline'}
+            size={24}
+            color="red"
+          />
+          <Text style={styles.likes}>{postData.likes}</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={onUnlikePressed} style={styles.actionButton}>
-          <Text style={styles.actionText}>Unlike</Text>
-        </TouchableOpacity>
+
         <TouchableOpacity
-          onPress={onCommentPressed}
-          style={styles.actionButton}
+          onPress={navigateToComments}
+          style={styles.commentButton}
         >
-          <Text style={styles.actionText}>Comment</Text>
+          <Ionicons name="chatbubble-outline" size={24} />
+          <Text style={styles.likes}>{postData.comments.length}</Text>
         </TouchableOpacity>
-        <Text style={styles.likes}>Likes: {postData.likes}</Text>
       </View>
+
+      {/* Image Viewer for Full-Screen View */}
+      <ImageViewer
+        images={postData.imageUrls.map((url) => ({ uri: url }))}
+        imageIndex={currentImageIndex}
+        visible={isViewerVisible}
+        onRequestClose={closeImageViewer}
+      />
     </View>
   );
 };
@@ -81,17 +201,23 @@ const styles = StyleSheet.create({
   container: {
     marginBottom: 16,
     padding: 16,
-    backgroundColor: '#fff',
-    borderRadius: 8,
+    backgroundColor: 'white',
     shadowColor: '#000',
-    shadowOpacity: 0.1,
-    shadowRadius: 4,
-    elevation: 2,
+    shadowOpacity: 0.9,
+    shadowRadius: 30,
+    elevation: 5,
   },
   userInfo: {
     flexDirection: 'row',
+    alignItems: 'center',
     justifyContent: 'space-between',
     marginBottom: 8,
+  },
+  avatar: {
+    width: 40,
+    height: 40,
+    borderRadius: 5,
+    marginRight: 16,
   },
   username: {
     fontWeight: 'bold',
@@ -103,43 +229,87 @@ const styles = StyleSheet.create({
   },
   content: {
     fontSize: 14,
-    marginBottom: 8,
+    marginVertical: 10,
   },
   imageContainer: {
     flexDirection: 'row',
     flexWrap: 'wrap',
     marginBottom: 8,
   },
-  image: {
-    width: 100,
-    height: 100,
-    marginRight: 8,
-    borderRadius: 8,
+  fullImageContainer: {
+    width: '100%',
+    marginBottom: 8,
   },
-  hashtags: {
+  twoImagesContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 8,
+  },
+  twoImageWrapper: {
+    width: '48%',
+  },
+  threeImagesContainer: {
+    flexDirection: 'row',
+    marginBottom: 8,
+  },
+  firstImageWrapper: {
+    width: '60%',
+  },
+  otherImagesWrapper: {
+    width: '40%',
+    flexDirection: 'column',
+  },
+  otherImageWrapper: {
+    width: '100%',
+    marginBottom: 8,
+  },
+  firstImage: {
+    width: '100%',
+    height: 200,
+    borderRadius: 8,
+    marginRight: 8,
+  },
+  otherImage: {
+    width: '100%',
+    height: 100,
+    borderRadius: 8,
+    marginLeft: 8,
+  },
+  hashtagsContainer: {
     flexDirection: 'row',
     flexWrap: 'wrap',
     marginBottom: 8,
   },
+  tag: {
+    backgroundColor: '#E0F7FF',
+    padding: 4,
+    borderRadius: 4,
+    marginRight: 4,
+    marginBottom: 4,
+  },
   hashtag: {
     color: '#007BFF',
-    marginRight: 4,
+    fontSize: 12,
   },
   actions: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
     alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: 8,
   },
-  actionButton: {
-    marginRight: 8,
-  },
-  actionText: {
-    color: '#007BFF',
+  likeButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginRight: 16,
   },
   likes: {
-    marginLeft: 'auto',
+    marginLeft: 8,
     color: '#666',
-    fontSize: 12,
+    fontSize: 16,
+  },
+  commentButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
 });
 
