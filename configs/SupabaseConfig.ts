7d3c150bc5215563e1c9f16e2b5159a17052419f
Quan <doquan020903@gmail.com>
2025-01-04 13:43:52 +0700

    feat: update supabase client for storage

--- configs/SupabaseConfig.ts
+++ configs/SupabaseConfig.ts
@@ -1,14 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL;
-const SUPABASE_PUBLIC_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.EXPO_PUBLIC_REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.EXPO_PUBLIC_REACT_APP_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(SUPABASE_URL!, SUPABASE_PUBLIC_KEY!, {
-  auth: {
-    storage: AsyncStorage,
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: false,
-  },
-});
+export const supabase = createClient(supabaseUrl!, supabaseKey!);
